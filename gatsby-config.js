8faf9ea2c264bef022f7c5db0337b93a5a70798a
Shane Jann <shanejann@gmail.com>
2019-11-20 16:00:41 -0600

    updated the config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Handshake Alliance Blog`,
+    author: `HandshakeAlliance`,
+    description: `Blog for the Handshake Alliance`,
+    siteUrl: `https://blog.handshakealliance.org`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `HNSAlliance`,
     },
   },
   plugins: [
