03a5173fafa46f9c845fdc693530be16cfe1da85
kilpatty <seanpkilgarriff@gmail.com>
2019-11-21 10:13:54 -0600

    blogpost: remove bio from footer

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
@@ -44,9 +43,7 @@ class BlogPostTemplate extends React.Component {
               marginBottom: rhythm(1),
             }}
           />
-          <footer>
-            <Bio />
-          </footer>
+          <footer></footer>
         </article>
 
         <nav>
